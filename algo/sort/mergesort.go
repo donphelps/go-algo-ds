dd56e12c14c2bfb95f85e4d99d34af05d0352e81
Don Phelps <don@phelps.nyc>
2017-12-22 10:08:12 -0500

    mergesort

---
+++ algo/sort/mergesort.go
@@ -0,0 +1,43 @@
+package sort
+
+import "fmt"
+
+/*
+Computational: O(n log(n))
+Additional memory use: O(n)
+*/
+
+// MergeSort initiates a Mergesort on the supplied int slice and returns a sorted int slice.
+func MergeSort(ints []int) []int {
+	if len(ints) < 2 {
+		return ints
+	}
+
+	mid := (len(ints)) / 2
+
+	return merge(MergeSort(ints[:mid]), MergeSort(ints[mid:]))
+}
+
+func merge(left, right []int) []int {
+	size, leftIndex, rightIndex := len(left)+len(right), 0, 0
+	slice := make([]int, size, size)
+
+	for i := 0; i < size; i++ {
+		if leftIndex > len(left)-1 && rightIndex <= len(right)-1 { // left is done, right is not.
+			slice[i] = right[rightIndex]
+			rightIndex++
+		} else if rightIndex > len(right)-1 && leftIndex <= len(left)-1 { // right is done, left is not.
+			slice[i] = left[leftIndex]
+			leftIndex++
+		} else if left[leftIndex] < right[rightIndex] { // next left value less than next right value.
+			slice[i] = left[leftIndex]
+			leftIndex++
+		} else {
+			slice[i] = right[rightIndex]
+			rightIndex++
+		}
+	}
+
+	fmt.Println(slice)
+	return slice
+}
---
+++ algo/sort/mergesort_test.go
@@ -0,0 +1,38 @@
+package sort
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func randomUnsortedInts(count int) []int {
+	slice := make([]int, count, count)
+
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < count; i++ {
+		slice[i] = rand.Intn(99999) - rand.Intn(99999)
+	}
+
+	return slice
+}
+
+func TestMergeSort(t *testing.T) {
+	goodSort := true
+	i := randomUnsortedInts(100)
+
+	sorted := MergeSort(i)
+
+	if len(sorted) > 2 {
+		for d := 0; d <= len(sorted)-2; d++ {
+			if sorted[d] >= sorted[d+1] {
+				goodSort = false
+			}
+		}
+	}
+
+	if !goodSort {
+		t.Errorf("MergeSort() result was %v", sorted)
+	}
+}
